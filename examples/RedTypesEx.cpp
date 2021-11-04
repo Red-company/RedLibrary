7fceadae95dde1c5aa856ce39e4a9a225dd14ff7
Vladimir Rogozin <vladimir20040609@gmail.com>
2021-11-04 21:36:11 +0300

    Editing 'RedTypesEx.txt'

--- examples/RedTypesEx.cpp
+++ examples/RedTypesEx.cpp
@@ -10,7 +10,14 @@ int main() {
     std::cout << "a: " << a << std::endl;
     std::cout << "b: " << b << std::endl;
 
+    // Red::uint512_t
+    // Red::uint1024_t
+    // Red::uint2048_t
+    // Red::uint4096_t
     Red::uint8192_t c = -1;
+    // Red::uint16384_t
+    // Red::uint32768_t
+    // Red::uint65536_t
 
     std::cout << std::endl << c << std::endl;
 
