10acb8592a8ad06f0aac8296276bd9d4044b2fc4
Mr.Red <77537827+vladimirrogozin@users.noreply.github.com>
2022-03-01 23:04:51 +0300

    Update ResECB1024Ex.cpp

--- examples/RedEncryptionAlgorithms/ResECB1024Ex.cpp
+++ examples/RedEncryptionAlgorithms/ResECB1024Ex.cpp
@@ -7,15 +7,20 @@ int main() {
     std::string key = "0123456789abcdef12345678123456780123456789abcdef12345678123456780123456789abcdef12345678123456780123456789abcdef1234567812345678";
     std::string in  = "0123456789abcdef";
 
-    std::string EncryptedStr = Red::EncryptResECB1024(in, key);
-    std::string EncryptedHex = Red::GetHexArray(EncryptedStr);
+    std::string *EncryptedStr = Red::EncryptResECB1024(in, key);
+    std::string *EncryptedHex = Red::GetHexArray(*EncryptedStr);
 
-    std::cout << "Encrypted(hex): '" << EncryptedHex << "'." << std::endl;
+    std::cout << "Encrypted(hex): '" << *EncryptedHex << "'." << std::endl;
 
-    std::string ForDecrypt = Red::GetStrArray(EncryptedHex);
-    std::string Decrypted  = Red::DecryptResECB1024(ForDecrypt, key);
+    std::string *ForDecrypt = Red::GetStrArray(*EncryptedHex);
+    std::string *Decrypted  = Red::DecryptResECB1024(*ForDecrypt, key);
 
-    std::cout << "Decrypted(str): '" << Decrypted << "'." << std::endl;
+    std::cout << "Decrypted(str): '" << *Decrypted << "'." << std::endl;
+    
+    delete EncryptedStr;
+    delete EncryptedHex;
+    delete ForDecrypt;
+    delete Decrypted;
 
     return 0;
 }
