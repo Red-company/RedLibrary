3d295097a12ac971da47de0d67fef326e8d628fc
Vladimir Rogozin <vladimir20040609@gmail.com>
2022-03-25 14:01:32 +0300

    RedTypes v1.3

--- RedTypes.h
+++ RedTypes.h
@@ -11,7 +11,7 @@
 
 #include <boost/multiprecision/cpp_int.hpp>
 
-#define REDTYPES_VERSION "1.2"
+#define REDTYPES_VERSION "1.3"
 
 namespace Red {
     // Standart size.
@@ -36,7 +36,18 @@ namespace Red {
     using uint131072_t = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<131072, 131072, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>, boost::multiprecision::et_off>;
     using uint262144_t = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<262144, 262144, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>, boost::multiprecision::et_off>;
     using uint524288_t = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<524288, 524288, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>, boost::multiprecision::et_off>;
-
+    using uint1048576_t = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<1048576, 1048576, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>, boost::multiprecision::et_off>;
+    using uint2097152_t = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<2097152, 2097152, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>, boost::multiprecision::et_off>;
+    using uint4194304_t = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<4194304, 4194304, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>, boost::multiprecision::et_off>;
+    using uint8388608_t = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<8388608, 8388608, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>, boost::multiprecision::et_off>;
+    using uint16777216_t = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<16777216, 16777216, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>, boost::multiprecision::et_off>;
+    using uint33554432_t = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<33554432, 33554432, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>, boost::multiprecision::et_off>;
+    using uint67108864_t = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<67108864, 67108864, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>, boost::multiprecision::et_off>;
+    using uint134217728_t = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<134217728, 134217728, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>, boost::multiprecision::et_off>;
+    using uint268435456_t = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<268435456, 268435456, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>, boost::multiprecision::et_off>;
+    using uint536870912_t = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<536870912, 536870912, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>, boost::multiprecision::et_off>;
+    using uint1073741824_t = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<1073741824, 1073741824, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>, boost::multiprecision::et_off>;
+    using uint2147483648_t = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<2147483648, 2147483648, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>, boost::multiprecision::et_off>;
 }
 
 #endif // RED_TYPES_H
