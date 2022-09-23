3797c1662fc34fc5d22be1fe618a16ad1f1499de
Vladimir Rogozin <vladimir20040609@gmail.com>
2022-09-22 23:48:57 -0400

    randomizer ex

--- examples/RandomizerEx.cpp
+++ examples/RandomizerEx.cpp
@@ -3,5 +3,24 @@
 #include "../RedLibrary/Randomizer.h"
 
 int main() {
-    std::cout << "Random num in [1; 2.000.000.000]: " << *Red::Randomize() % 2000000000 + 1 << std::endl;
+    // Randomize().
+
+    unsigned long long int *rand1 = Red::Randomize();
+
+    *rand1 = *rand1 % 2000000000 + 1;
+
+    std::cout << "Random num in [1; 2.000.000.000]: " << *rand1 << std::endl << std::endl;
+
+    delete rand1;
+
+
+    // Randomize_custom().
+
+    unsigned long long int len = 17;
+
+    Red::bignum_t *rand2 = Red::Randomizer_custom(&len);
+
+    std::cout << "Random num of len=17: " << *rand2 << std::endl;
+
+    delete rand2;
 }
