f323a911f74cf98387add57f27fb1cabbb9dba1d
Vladimir Rogozin <vladimir20040609@gmail.com>
2022-10-16 23:23:30 -0400

    -

--- examples/2layerDiffieHellmanEx.cpp
+++ examples/2layerDiffieHellmanEx.cpp
@@ -7,26 +7,27 @@
 #include "../RedLibrary/RedTypes.h"
 
 int main() {
-    Red::uint65536_t P = -1, a1, a2, b1, b2;
+    Red::bignum_t P = 1, a1, a2, b1, b2;
     srand(time(0));
 
     // Generating secrets.
-    a1 = rand() % RED_2lDH_RANDOM_A1_KEY_70m; //rand() % RED_2lDH_RANDOM_A1_KEY_70m + 2; // also there're _105m, _126m, _238m, _336m.
-    a2 = 6000;
+    a1 = rand() % RED_2lDH_RANDOM_A1_KEY_70 + 2; // also there're 100, 140.
+    a2 = rand() % RED_2lDH_RANDOM_A1_KEY_70 + 2;
 
-    b1 = 2; // rand() % RED_2lDH_RANDOM_A1_KEY_70m + 2;
-    b2 = 6000;
+    b1 = rand() % RED_2lDH_RANDOM_A1_KEY_70 + 2;
+    b2 = rand() % RED_2lDH_RANDOM_A1_KEY_70 + 2;
 
-    Red::TwoLayerDiffieHellman<Red::uint65536_t> Alice(&P, &a1, &a2, RED_2lDH_6k_AUTO); // Also there're _8k_, _11k_, _16k_, _20k_.
-    Red::TwoLayerDiffieHellman<Red::uint65536_t> Bob(&P, &b1, &b2); // Will set 8k automatically.
+
+    Red::TwoLayerDiffieHellman Alice(&P, &a1, &a2);
+    Red::TwoLayerDiffieHellman Bob(RED_2lDH_P_UNLIMITED, &b1, &b2); // same to P = 1;
 
     std::cout << std::endl;
     std::cout << "The private keys('a1 : a2') for Alice : " << a1 << " : " << a2 << std::endl;
     std::cout << "The private keys('b1 : b2') for Bob   : " << b1 << " : " << b2 << std::endl << std::endl;
 
     // Generating public keys.
-    Red::uint65536_t *Pa = Alice.Part1_GetPublicValue();
-    Red::uint65536_t *Pb = Bob.Part1_GetPublicValue();
+    Red::bignum_t *Pa = Alice.Part1_GetPublicValue();
+    Red::bignum_t *Pb = Bob.Part1_GetPublicValue();
 
     std::cout << "Pa: " << *Pa << std::endl << std::endl;
     std::cout << "Pb: " << *Pb << std::endl << std::endl;
@@ -38,12 +39,12 @@ int main() {
     auto start = std::chrono::system_clock::now();
 
     // Generating a num.
-    Red::uint65536_t *ValueA = Alice.Part2_GetPublicValue();
-    Red::uint65536_t *ValueB = Bob.Part2_GetPublicValue();
+    Red::bignum_t *ValueA = Alice.Part2_GetPublicValue();
+    Red::bignum_t *ValueB = Bob.Part2_GetPublicValue();
 
     // Getting the result pair.
-    Red::uint65536_t *Sa = Alice.Part2_GetSymmetricSecret(ValueB);
-    Red::uint65536_t *Sb = Bob.Part2_GetSymmetricSecret(ValueA);
+    Red::bignum_t *Sa = Alice.Part2_GetSymmetricSecret(ValueB);
+    Red::bignum_t *Sb = Bob.Part2_GetSymmetricSecret(ValueA);
 
     auto duration = (std::chrono::system_clock::now() - start) * 0.000001;
 
