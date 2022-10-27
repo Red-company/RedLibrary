4ccb26b3a1ecc04d33a5c58c4058bf0f25e369a4
Vladimir Rogozin <vladimir20040609@gmail.com>
2022-10-27 00:17:34 -0400

    undo

--- RedTypes.h
+++ RedTypes.h
@@ -101,9 +101,9 @@ namespace Red {
              */
             BigInt(std::string& s) {
                 digits = "";
-                unsigned long int n = s.size();
+                unsigned int n = s.size();
 
-                for (unsigned long int i = n - 1; i >= 0; i--) {
+                for (unsigned int i = n - 1; i >= 0; i--) {
                     if (!isdigit(s[i])) {
                         throw("ERROR");
                     }
@@ -120,7 +120,7 @@ namespace Red {
             BigInt(const char *s) {
                 digits = "";
 
-                for (unsigned long int i = strlen(s) - 1; i >= 0; i--) {
+                for (unsigned int i = strlen(s) - 1; i >= 0; i--) {
                     if (!isdigit(s[i])) {
                         throw("ERROR");
                     }
